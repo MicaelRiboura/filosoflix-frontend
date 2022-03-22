4b8bb7fcdfaafe9d868c4cc7ade706536a325e06
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-22 17:52:08 -0300

    feat: add responsivity in card video component

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -12,9 +12,20 @@ export const Container = styled.div<CardProps>`
     margin-right: 100px;
     transition: .2s;
     border: 2px solid var(--dark);
-
     &:hover{
-       border: 2px solid var(--primary);
-       transform: scale(1);
+        border: 2px solid var(--primary);
+        transform: scale(1);
     }
-`;
+
+    @media(max-width: 1024px) {
+        width: 300px;
+        height: 150px;
+        
+    }
+    
+    @media(max-width: 480px) {
+        width: 100%;
+        height: 200px;
+        
+    }
+    `;
