90e9af5330e386e433dd057d9e1dc4b9fb33a994
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-23 15:55:11 -0300

    feat: adjust responsivity related with option attribute in card component

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
-import { CardProps } from './index'
+import { CardOptions, CardProps } from './index'
 
 export const Container = styled.div<CardProps>`
     cursor: pointer;
     width: 100%;
-    height: 158px;
+    height: ${props => props.option === CardOptions.videosdetails ? '200px' : '158px'};
     background-color: var(--dark);
     background-image: url(${props => props.thumbImg});
     background-size: cover;
     background-position: 50%;
     margin-right: 100px;
+    margin-top: ${props => props.option === CardOptions.videosdetails ? '1rem' : '0px'};
     transition: .2s;
     border: 2px solid var(--dark);
     &:hover{
@@ -17,15 +18,25 @@ export const Container = styled.div<CardProps>`
         transform: scale(1);
     }
 
-    @media(max-width: 1024px) {
-        width: 300px;
-        height: 150px;
-        
+    @media(max-width: 1440px) {
+        width: ${props => props.option === CardOptions.videosdetails ? '100%' : '300px'};
+        height: ${props => props.option === CardOptions.videosdetails ? '250px' : '158px'};
+
     }
-    
+    @media(max-width: 1024px) {
+        width: ${props => props.option === CardOptions.videosdetails ? '100%' : '300px'};
+        height: ${props => props.option === CardOptions.videosdetails ? '600px' : '180px'};
+
+    }
+    @media(max-width: 768px) {
+        width: ${props => props.option === CardOptions.videosdetails ? '100%' : '300px'};
+        height: ${props => props.option === CardOptions.videosdetails ? '400px' : '170px'};
+
+    }
+
     @media(max-width: 480px) {
         width: 100%;
         height: 200px;
-        
+
     }
     `;
