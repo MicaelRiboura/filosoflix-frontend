60126a8d30e4cc5fbc940015a5b2105e5298413f
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-23 15:54:35 -0300

    feat: add option attribute in card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Container } from './styles';
 
+export enum CardOptions {
+    list,
+    videosdetails
+};
 export interface CardProps {
     thumbImg: string;
+    option?: CardOptions;
 }
 
-const Card: React.FC<CardProps> = ({ thumbImg }) => {
+const Card: React.FC<CardProps> = ({ thumbImg, option = CardOptions.list}) => {
     return (
-        <Container thumbImg={thumbImg}></Container>
+        <Container thumbImg={thumbImg} option={option}></Container>
     );
 }
 
